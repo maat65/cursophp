f3e3c50a5aa715e2a93f85eb62053c56c7ba4fc4
Mateus Yoon <mateusyoon0605@hotmail.com>
2024-08-27 14:01:01 -0300

    Antecessor e Sucessor

---
+++ desaf001/resposta.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antecessor e Sucessor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Antecessor e Sucessor do Número</h1>
+    </header>
+    <main>
+        <?php 
+            $num = $_GET["numero"];
+            $antecessor = $num - 1;
+            $sucessor = $num + 1;
+            echo("O antecessor do número <strong>$num</strong> é <strong>$antecessor</strong><br>");
+            echo("O sucessor do número <strong>$num</strong> é <strong>$sucessor</strong><br>");
+        ?>
+    </main>
+</body>
+</html>
