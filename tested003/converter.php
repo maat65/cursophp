35e13e71b3a5524edbe526fe76018fec52ba0d5f
Mateus Yoon <mateusyoon0605@hotmail.com>
2024-08-27 15:02:44 -0300

    Conversão de moedas solo

---
+++ tested003/converter.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversão de real para dólar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Valor convertido do Real para Dólar</h1>
+        <?php 
+            $cotacao = 5.51;
+            $valor = $_GET["real"];
+            $dolar = $valor / $cotacao;
+
+            echo("<p>O valor <strong>R\$" . number_format($valor, 2, ",", ".") . "</strong> equivale a um total de <strong>U\$" . number_format($dolar, 2, ",", ".") ."</strong> dólares");
+    
+        ?>
+    </main>
+</body>
+</html>
