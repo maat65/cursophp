53353e8f26483d0e89aa72c3c23038b7291923ee
Mateus Yoon <mateusyoon0605@hotmail.com>
2024-08-27 14:32:40 -0300

    Antecessor e sucessor solo

---
+++ tested001/resposta.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta do antecessor e sucessor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Antecessor e Sucessor do número</h1>
+    <main>
+        <?php 
+            $num = $_GET["numero"];
+            $antecessor = $num - 1;
+            $sucessor = $num + 1;
+
+            echo("O número <strong>$num</strong> possui como antecessessor o número <strong>$antecessor</strong> e como sucessor o número <strong>$sucessor</strong>");
+    
+    
+        ?>
+    </main>
+    
+</body>
+</html>
