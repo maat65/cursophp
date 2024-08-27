abe6fc12263c9794078c4396281c95ed117540fd
Mateus Yoon <mateusyoon0605@hotmail.com>
2024-08-27 18:54:06 -0300

    Ajustando alguns detalhes salário minimo solo

--- dphp002/index.php
+++ dphp002/index.php
@@ -8,26 +8,25 @@
 </head>
 <body>
     <?php 
-        $minimo = 1380;
-        $salario = $_GET["salario"];
-        $numero = $salario / $minimo;
-        $resto = $salario - ((int)$numero * $minimo);
+        $minimo = 1380.60;
+        $salario = $_GET["salario"] ?? 0;
     ?>
     <main>
         <h1>Informe seu salário</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="salario">Salário (R$)</label>
             <input type="number" name="salario" id="salario" step="0.01">
-            <p>Considerando o salário mínimo de <strong>R$1.380,00</strong></p>
+            <p>Considerando o salário mínimo de <strong>R$<?=number_format($minimo, 2, ",", ".")?></strong></p>
             <input type="submit" value="Calcular">
         </form>
     </main>
 
     <section>
         <?php 
+            $numero = intdiv($salario, $minimo); // $numero = $salario / $minimo;
+            $resto = $salario % $minimo; // $resto = $salario - ((int)$numero * $minimo);
             echo("<h1>Resultado Final</h1>");
-            echo("Quem recebe um salário de R\$1.380,00 ganha <strong>" . (int)$numero . " salários mínimos + R\$" . number_format($resto, 2, ",", "."));
-        
+            echo("<p>Quem recebe um salário de R\$" . number_format($salario, 2, ",", ".") . " ganha <strong>" . (int)$numero . " salários mínimos + R\$" . number_format($resto, 2, ",", ".") . "</strong></p>");
         ?>
     </section>
     
